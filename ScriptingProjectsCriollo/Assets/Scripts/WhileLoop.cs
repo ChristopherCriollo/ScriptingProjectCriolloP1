ed751d1b051fe02bd44478837d207df91655a41c
ChristopherCriollo <718675@student.dusd.net>
2022-11-01 09:57:12 -0700

    Loops
    Finish Loops

---
+++ ScriptingProjectsCriollo/Assets/Scripts/WhileLoop.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ForLoop : MonoBehaviour
+{
+    int cupsInTheSink = 4;
+
+
+    void Start()
+    {
+        while (cupsInTheSink > 0)
+        {
+            Debug.Log("I've washed a cup!");
+            cupsInTheSink--;
+        }
+    }
+}
